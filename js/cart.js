d3cbef21d080a6bf1a43d943af727bccc0d27189
alexibarrauy <110476901+alexibarrauy@users.noreply.github.com>
2022-10-13 21:09:55 -0300

    Parte 1 entrega 5
    SIIIUUU

---
+++ js/cart.js
@@ -0,0 +1,46 @@
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetch(CART_INFO_URL + "25801.json")
+    .then(respuesta => respuesta.json())
+    .then(datos =>{
+        let Info = datos.articles[0]
+        let InfoCart = "";
+        InfoCart += `
+        <tr>
+            <b>
+                <th scope="col"><img src="${Info.image}" class="image"></img></th>
+                <th scope="col">${Info.name}</th>
+                <th scope="col">${Info.unitCost}</th>
+                <th scope="col"><input type="number" min="1" id="cantidad" placeholder="1"></th>
+                <th scope="col" id="subtotal">${Info.unitCost}</th>
+            </b>
+        </tr>`
+        document.getElementById("tabla_cart").innerHTML += InfoCart
+        
+        let cantidad = document.getElementById("cantidad")
+        let subtotal = document.getElementById("subtotal")
+
+        document.getElementById("cantidad").addEventListener("change", ()=>{
+            let preciofinal = "";
+            preciofinal = parseInt(cantidad.value)*parseInt(Info.unitCost)
+            
+            console.log(preciofinal)
+            subtotal.innerHTML = preciofinal
+        })
+         
+        
+
+
+    })
+})
+// el lado oscuro de la fuerza
+
+// .addEventListener("click", ()=>{
+//     let preciofinal = "";
+//     preciofinal = `
+//     (${cantidad} * ${subtotal})`
+//     console.log(preciofinal)
+// })
+
+
+    
