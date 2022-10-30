9e39f19064543cace7cb49fcc53fdebdd99505ea
alexibarrauy <110476901+alexibarrauy@users.noreply.github.com>
2022-10-30 14:47:49 -0300

    Entrega 5 cambios finales
    Me olvide de subirlos xdxd

--- js/cart.js
+++ js/cart.js
@@ -4,15 +4,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
     .then(respuesta => respuesta.json())
     .then(datos =>{
         let Info = datos.articles[0]
+        console.log(Info)
         let InfoCart = "";
         InfoCart += `
         <tr>
             <b>
                 <th scope="col"><img src="${Info.image}" class="image"></img></th>
                 <th scope="col">${Info.name}</th>
-                <th scope="col">${Info.unitCost}</th>
+                <th scope="col">${Info.unitCost} ${Info.currency}</th>
                 <th scope="col"><input type="number" min="1" id="cantidad" placeholder="1"></th>
-                <th scope="col" id="subtotal">${Info.unitCost}</th>
+                <th scope="col" id="subtotal">${Info.unitCost} ${Info.currency}</th>
             </b>
         </tr>`
         document.getElementById("tabla_cart").innerHTML += InfoCart
@@ -25,7 +26,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
             preciofinal = parseInt(cantidad.value)*parseInt(Info.unitCost)
             
             console.log(preciofinal)
-            subtotal.innerHTML = preciofinal
+            subtotal.innerHTML = preciofinal + ` ` + Info.currency
         })
          
         
