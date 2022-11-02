b9d9b72ae2ae264ff2aa71d69e1fd408934841d4
alexibarrauy <110476901+alexibarrauy@users.noreply.github.com>
2022-11-02 18:17:40 -0300

    Entrega 6 casi completa
    Falta verificacion modal

--- js/cart.js
+++ js/cart.js
@@ -1,23 +1,4 @@
-let UsuarioHTML = document.getElementById('NombreUsuario')// Aca se carga el texto de arriba segun que categoria visitemos.
-let contenido2 = `
-<div class="dropdown">
-<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    ${localStorage.getItem('usuario')}
-</button>
-<ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
-    <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
-    <li id="cerrar_sesion"><a class="dropdown-item" href="index.html">Cerrar sesión</a></li>
-</ul>
-</div>
-    `
-UsuarioHTML.innerHTML = contenido2
-
-document.getElementById('cerrar_sesion').addEventListener('click', function(){
-localStorage.removeItem('usuario')
-}) 
-
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", function(){
     fetch(CART_INFO_URL + "25801.json")
     .then(respuesta => respuesta.json())
     .then(datos =>{
@@ -25,39 +6,85 @@ document.addEventListener("DOMContentLoaded", ()=>{
         console.log(Info)
         let InfoCart = "";
         InfoCart += `
-        <tr>
-            <b>
-                <th scope="col"><img src="${Info.image}" class="image"></img></th>
-                <th scope="col">${Info.name}</th>
-                <th scope="col">${Info.unitCost} ${Info.currency}</th>
-                <th scope="col"><input type="number" min="1" id="cantidad" placeholder="1"></th>
-                <th scope="col" id="subtotal">${Info.unitCost} ${Info.currency}</th>
-            </b>
-        </tr>`
-        document.getElementById("tabla_cart").innerHTML += InfoCart
-        document.getElementById("productCostText").innerHTML = Info.unitCost + " " + Info.currency 
+            <tr>
+                <b>
+                    <th scope="col"><img src="${Info.image}" class="image"></img></th>
+                    <th scope="col">${Info.name}</th>
+                    <th scope="col">${Info.unitCost} ${Info.currency}</th>
+                    <th scope="col"><input class="col-md-5" type="number" min="1" id="cantidad" value="1"></th>
+                    <th scope="col" id="subtotal">${Info.unitCost} ${Info.currency}</th>
+                </b>
+            </tr>
+            `
+        document.getElementById("tabla_cart").innerHTML += InfoCart // primera tabla
         
-        let cantidad = document.getElementById("cantidad")
         let subtotal = document.getElementById("subtotal")
+        let subtotal2 = document.getElementById("costoUnitario")
+        let total = document.getElementById("costoTotal")
+        let cantidad = document.getElementById("cantidad")
+        let comision = 0.05
+        let Pcomision = document.getElementById("segunEnvio")    
+        let preciofinal = parseInt(cantidad.value)*parseInt(Info.unitCost)
 
-        document.getElementById("cantidad").addEventListener("change", ()=>{
-            let preciofinal = "";
+        function CambiarPrecios(){ //function para hallar precios
             preciofinal = parseInt(cantidad.value)*parseInt(Info.unitCost)
-            
+            subtotal.innerHTML = preciofinal + " " + Info.currency
+            subtotal2.innerHTML = preciofinal + " " + Info.currency
+            Pcomision.innerHTML = Math.round(preciofinal * comision) + " " + Info.currency    
+            total.innerHTML = preciofinal + (Info.unitCost * comision) + " " + (Info.currency)
+        }
+
+        CambiarPrecios()
+        
+       
+        document.getElementById("cantidad").addEventListener("change", ()=>{
             console.log(preciofinal)
-<<<<<<< HEAD
-            subtotal.innerHTML = preciofinal + ` ` + Info.currency
-=======
-            subtotal.innerHTML = preciofinal + ` ` + Info.currency 
->>>>>>> 9c2608e6e3481ef5905ff53cdad28e8f6818ab86
+            if (cantidad.value <= 1){
+                cantidad.value = 1
+            }
+            CambiarPrecios()
+            
+
         })
-        
-         
+
         
 
-
+        document.getElementById("premium").addEventListener("change", function(){
+            comision = 0.15;
+            CambiarPrecios()
+        });
+        
+        document.getElementById("express").addEventListener("change", function(){
+            comision = 0.07;
+            CambiarPrecios()
+        });
+        
+        document.getElementById("standard").addEventListener("change", function(){
+            comision = 0.05;
+            CambiarPrecios()
+        });
+                
     })
-})
+});
+
+(() => { // validacion
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
 
 
 // el lado oscuro de la fuerza
@@ -69,5 +96,3 @@ document.addEventListener("DOMContentLoaded", ()=>{
 //     console.log(preciofinal)
 // })
 
-
-    
